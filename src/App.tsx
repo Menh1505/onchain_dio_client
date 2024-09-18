e5b733385d91d54f803f8489db2a5eb023bae071
Menhythien <dinhthienmenh1505@gmail.com>
2024-09-18 22:16:40 +0700

    fix package

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,3 @@
-
 import { WalletSelector } from "@aptos-labs/wallet-adapter-ant-design";
 import "@aptos-labs/wallet-adapter-ant-design/dist/index.css";
 import { useRef, useState, useEffect } from "react";
